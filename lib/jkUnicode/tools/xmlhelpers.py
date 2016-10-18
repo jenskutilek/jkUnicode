830ee1a81f79883557555df1977db128ba4210f0
Jens Kutilek <webmail@kutilek.de>
2016-10-18 18:35:04 +0200

    Bugfix

--- lib/jkUnicode/tools/xmlhelpers.py
+++ lib/jkUnicode/tools/xmlhelpers.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 # The dirty hacky stuff is outsourced into this file
 
 from re import compile
@@ -15,7 +17,7 @@ def split_all(l):
 	return r
 
 
-def filtered_char_list(xml_char_list):
+def filtered_char_list(xml_char_list, debug=False):
 	# Filter backslashes and other peculiarities of the XML format from the character list
 	c = xml_char_list
 	
@@ -25,10 +27,10 @@ def filtered_char_list(xml_char_list):
 	
 	if xml_char_list[0] == "[" and xml_char_list[-1] == "]":
 		xml_char_list = xml_char_list[1:-1]
-	#print "Step 1: ****%s****" % xml_char_list
+	if debug: print "Step 1: ****%s****" % xml_char_list
 	
 	xml_char_list = xml_char_list.split()
-	#print "Step 2: %s" % xml_char_list
+	if debug: print "Step 2: %s" % xml_char_list
 	
 	for d in xml_char_list:
 		if d[0] == "{":
@@ -36,7 +38,7 @@ def filtered_char_list(xml_char_list):
 		if d[-1] == "}":
 			d = d[:-1]
 		e = split_all(d)
-		#print "e =", e
+		if debug: print "e =", e
 		filtered1.extend(e)
 	
 	# Remove duplicate entries
@@ -52,16 +54,19 @@ def filtered_char_list(xml_char_list):
 		m = ur.search(c)
 		if m:
 			c = getUnicodeChar(int(m.groups(0)[0], 16))
-		filtered.append(c)
-	#print filtered
+		else:
+			if len(c) > 1:
+				c = [d for d in c]
+		filtered.extend(c)
+	if debug: print filtered
 	return sorted(filtered)
 
 
 if __name__ == "__main__":
 	lists = [
-		r"[\u200C\u200D \u200F A {A\u0301} {E \u0302} \]]"
+		r"[\u200C\u200D \u200F A {A\u0301} {E \u0302} {ij} \]]"
 	]
 
 	for cl in lists:
-		ll = filtered_char_list(cl)
+		ll = filtered_char_list(cl, True)
 		print "Result:", ll
