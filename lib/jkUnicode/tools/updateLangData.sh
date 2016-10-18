2b625817003c06ee234581421f5dbb362169b24b
Jens Kutilek <webmail@kutilek.de>
2016-10-18 15:10:48 +0200

    Add script for downloading CLDR data

---
+++ lib/jkUnicode/tools/updateLangData.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Check the URL if it is still current:
+# http://unicode.org/Public/cldr/
+# There seems to be no stable URL for the current version.
+curl -O http://unicode.org/Public/cldr/30.0.2/core.zip
+unzip core.zip -d core/
