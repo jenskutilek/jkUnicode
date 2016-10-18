86066d7b31090a0ab50a635935a59dc1aec73126
Jens Kutilek <webmail@kutilek.de>
2016-10-18 18:30:28 +0200

    Put hacky function into separate file with tests

--- lib/jkUnicode/tools/generatePyLangData.py
+++ lib/jkUnicode/tools/generatePyLangData.py
@@ -3,6 +3,8 @@
 
 import codecs, copy, json, os
 import xml.etree.ElementTree as ET
+from xmlhelpers import filtered_char_list
+
 
 xml_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "core", "common", "main")
 en_path  = os.path.join(xml_path, "en.xml")
@@ -25,50 +27,51 @@ def json_to_file(path, file_name, obj):
 		json.dump(obj, f, ensure_ascii=False, indent=4, sort_keys=True)
 
 
+#def generate_language_data():
 if not(os.path.exists(en_path)):
 	print "XML language data not found.\nPlease use the shell script 'updateLangData.sh' to download it."
 else:
 	# Extract language names from the English data file
 
 	root = ET.parse(en_path).getroot()
-	
+
 	language_dict = extract_dict(root, "localeDisplayNames/languages/language")
 	print "OK: Read %i language names." % len(language_dict)
 
 	script_dict = extract_dict(root, "localeDisplayNames/scripts/script")
 	print "OK: Read %i script names." % len(script_dict)
-	
+
 	json_path = os.path.join(os.path.split(os.path.dirname(os.path.realpath(__file__)))[0], "json")
-	
+
 	json_to_file(json_path, "languages", language_dict)
 	json_to_file(json_path, "scripts", script_dict)
-	
+
 	print "Parsing language character data ..."
-	
+
 	language_chars = {}
 	ignored_languages = copy.deepcopy(language_dict)
 	file_not_found = []
 	i = 0
-	
+
 	for code, name in language_dict.items():
-		i += 1
 		char_dict = {}
 		lang_xml_path = os.path.join(xml_path, "%s.xml" % code)
 		if not os.path.exists(lang_xml_path):
 			#print "Not found:", lang_xml_path
 			file_not_found.append(code)
 		else:
+			i += 1
+			#print code, name
 			root = ET.parse(lang_xml_path).getroot()
 			ec = root.findall("characters/exemplarCharacters")
 			for c in ec:
 				if c.attrib == {}:
 					# Main entry
-					char_dict["main"] = c.text.strip("[]").split()
+					char_dict["main"] = filtered_char_list(c.text)
 				elif "type" in c.attrib:
 					t = c.attrib["type"]
 					if t in ["auxiliary", "punctuation"]:
-						char_list = c.text.strip("[]").split()
-						char_dict[c.attrib["type"]] = char_list
+						char_dict[c.attrib["type"]] = filtered_char_list(c.text)
 		if char_dict:
 			language_chars[code] = {"name": name, "characters": char_dict}
 			del ignored_languages[code]
---
+++ lib/jkUnicode/tools/xmlhelpers.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# The dirty hacky stuff is outsourced into this file
+
+from re import compile
+from jkUnicode import getUnicodeChar
+
+ur = compile("^u([0-9A-F]+)$") # Regex to match unicode sequences, e.g. \u0302
+
+
+def split_all(l):
+	a = l.split()
+	r = []
+	for b in a:
+		r.extend(b.split("\\"))
+	return r
+
+
+def filtered_char_list(xml_char_list):
+	# Filter backslashes and other peculiarities of the XML format from the character list
+	c = xml_char_list
+	
+	filtered1 = []
+	filtered = []
+	
+	
+	if xml_char_list[0] == "[" and xml_char_list[-1] == "]":
+		xml_char_list = xml_char_list[1:-1]
+	#print "Step 1: ****%s****" % xml_char_list
+	
+	xml_char_list = xml_char_list.split()
+	#print "Step 2: %s" % xml_char_list
+	
+	for d in xml_char_list:
+		if d[0] == "{":
+			d = d[1:]
+		if d[-1] == "}":
+			d = d[:-1]
+		e = split_all(d)
+		#print "e =", e
+		filtered1.extend(e)
+	
+	# Remove duplicate entries
+	filtered1 = list(set(filtered1))
+	
+	# Remove empty entries
+	try:
+		filtered1.remove("")
+	except ValueError:
+		pass
+	
+	for c in filtered1:
+		m = ur.search(c)
+		if m:
+			c = getUnicodeChar(int(m.groups(0)[0], 16))
+		filtered.append(c)
+	#print filtered
+	return sorted(filtered)
+
+
+if __name__ == "__main__":
+	lists = [
+		r"[\u200C\u200D \u200F A {A\u0301} {E \u0302} \]]"
+	]
+
+	for cl in lists:
+		ll = filtered_char_list(cl)
+		print "Result:", ll
