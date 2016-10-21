14f6158bc6107e3437e9d5a18ce66aafc8c3919a
Jens Kutilek <webmail@kutilek.de>
2016-10-21 11:51:24 +0200

    Add option to save space in JSON output

--- lib/jkUnicode/tools/jsonhelpers.py
+++ lib/jkUnicode/tools/jsonhelpers.py
@@ -2,9 +2,13 @@ import codecs, json, os
 
 json_path = os.path.join(os.path.split(os.path.dirname(os.path.realpath(__file__)))[0], "json")
 
-def json_to_file(path, file_name, obj):
+def json_to_file(path, file_name, obj, human_readable=True):
 	with codecs.open(os.path.join(path, "%s.json" % file_name), "w", "utf-8") as f:
-		json.dump(obj, f, ensure_ascii=False, indent=4, sort_keys=True)
+		if human_readable:
+			indent = 4
+		else:
+			indent = None
+		json.dump(obj, f, ensure_ascii=False, indent=indent, sort_keys=True)
 
 def dict_from_file(path, file_name):
 	with codecs.open(os.path.join(path, "%s.json" % file_name), "r", "utf-8") as f:
