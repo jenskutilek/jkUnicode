cb369ec287476dabf7ecc691d1c3a46293e88ed1
Jens Kutilek <webmail@kutilek.de>
2017-01-05 12:48:45 +0100

    Version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 	name = "jkUnicode",
-	version = "1.1",
+	version = "1.1.0",
 	description = "Unicode support libraries.",
 	author = "Jens Kutilek",
 	url = "http://www.kutilek.de/",
