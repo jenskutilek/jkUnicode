7d9fcd2a6d8c7e3f36746115f67cbcb975083aaf
Jens Kutilek <webmail@kutilek.de>
2017-01-05 11:50:01 +0100

    Improve setup.py for internal deployment

--- setup.py
+++ setup.py
@@ -3,14 +3,26 @@
 from distutils.core import setup
 
 setup(
-		name = "jkUnicode",
-		version = "1.0",
-		description = "Unicode support libraries.",
-		author = "Jens Kutilek",
-		url = "http://www.kutilek.de/",
-		packages = [
-			"jkUnicode",
-			#"jkUnicode.tools",
-		],
-		package_dir = {"": "lib"},
-	)
+	name = "jkUnicode",
+	version = "1.1",
+	description = "Unicode support libraries.",
+	author = "Jens Kutilek",
+	url = "http://www.kutilek.de/",
+	packages = [
+		"jkUnicode",
+		"jkUnicode.fonttools",
+		"jkUnicode.tools",
+	],
+	package_dir = {"": "lib"},
+	package_data = {"": [
+		"json/ignored.json",
+		"json/language_characters.json",
+		"json/languages.json",
+		"json/scripts.json",
+		#"json/tags.json",
+		"json/territories.json",
+	]},
+	requires = [
+		"fontTools",
+	],
+)
