0c8bd776eb0ae6150f04fe550dce1d9ba05025d9
Jens Kutilek <webmail@kutilek.de>
2017-01-20 12:20:23 +0100

    Streamline cmap processing

--- lib/jkUnicode/fonttools/sfnt.py
+++ lib/jkUnicode/fonttools/sfnt.py
@@ -3,15 +3,11 @@
 
 def get_cmap(font):
 	# Get the preferred cmap subtable from a fontTools.ttLib.TTFont
-	table = font["cmap"]
 	
-	cmap = {}
-	cmapIDs = [(3, 10), (0, 3), (3, 1)]
-	for i in range(len(cmapIDs)):
-		if table.getcmap(cmapIDs[i][0], cmapIDs[i][1]):
-			cmap = table.getcmap(cmapIDs[i][0], cmapIDs[i][1]).cmap
-			break
-	if not cmap:
-		print "ERROR extracting codepoints from font. Found neither CMAP (3, 10), (0, 3), nor (3, 1)."
-	return cmap
+	for platformID, encodingID in [(3, 10), (0, 3), (3, 1)]:
+		cmapSubtable = font["cmap"].getcmap(platformID, encodingID)
+		if cmapSubtable is not None:
+			return cmapSubtable.cmap
+	print "ERROR extracting codepoints from font. Found neither CMAP (3, 10), (0, 3), nor (3, 1)."
+	return {}
 
