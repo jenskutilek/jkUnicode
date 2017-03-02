3c9112ec15c3fd112aa45bf0a66b5359129683b4
Jens Kutilek <webmail@kutilek.de>
2017-03-02 17:56:58 +0100

    Catch more Cyrillic edge cases

--- lib/jkUnicode/uniNiceName.py
+++ lib/jkUnicode/uniNiceName.py
@@ -1,31 +1,55 @@
 import re
 
-#re_small_letter   = re.compile("^(.+SMALL LETTER )([A-Z ]+?)( WITH.+)?$")
-re_capital_letter = re.compile("^(.+CAPITAL LETTER )([A-Z ]+?)( WITH.+)?$")
+re_small_letter   = re.compile("^(.+SMALL LETTER )([A-Z\- ]+?)( WITH.+)?$")
+re_capital_letter = re.compile("^(.+CAPITAL LETTER )([A-Z\- ]+?)( WITH.+)?$")
 re_allah = re.compile("^(.+?)(ALLAH)(.+)?$")
 
 letter_names = {
 	"AE": "AE",
 }
 
-#def transform_small_letter(name):
-#	#print "transform_small_letter", name
-#	m = re_small_letter.match(name)
-#	if m:
-#		#print m.groups()
-#		result = "%s%s" % (m.group(1).capitalize(), str(m.group(2)).lower())
-#		if m.group(3) is not None:
-#			result += "%s" % m.group(3).lower()
-#		#print "Result:", result
-#		return result
-#	return False
+def transform_small_letter(name):
+	#print "transform_small_letter", name
+	m = re_small_letter.match(name)
+	if m:
+		#print m.groups()
+		result = m.group(1).capitalize()
+		if " " in m.group(2):
+			parts = m.group(2).split()
+			if parts[0] in ("UKRAINIAN", "BYELORUSSIAN-UKRAINIAN"):
+				parts[0] = parts[0].title()
+				for i in range(1, len(parts)):
+					parts[i] = parts[i].lower()
+				t_name = " ".join(parts)
+				result += t_name
+			else:
+				result += letter_names.get(m.group(2), m.group(2).lower())
+		else:	
+			result += letter_names.get(m.group(2), m.group(2).lower())
+		if m.group(3) is not None:
+			result += "%s" % m.group(3).lower()
+		#print "Result:", result
+		return result
+	return False
 
 def transform_capital_letter(name):
 	#print "transform_capital_letter", name
 	m = re_capital_letter.match(name)
 	if m:
 		#print m.groups()
-		result = "%s%s" % (m.group(1).capitalize(), letter_names.get(m.group(2), m.group(2).title()))
+		result = m.group(1).capitalize()
+		if " " in m.group(2):
+			parts = m.group(2).split()
+			if parts[0] in ("SHORT", "STRAIGHT"):
+				parts[0] = parts[0].lower()
+				for i in range(1, len(parts)):
+					parts[i] = parts[i].title()
+				t_name = " ".join(parts)
+				result += t_name
+			else:
+				result += letter_names.get(m.group(2), m.group(2).title())
+		else:	
+			result += letter_names.get(m.group(2), m.group(2).title())
 		if m.group(3) is not None:
 			result += "%s" % m.group(3).lower()
 		#print "Result:", result
@@ -44,7 +68,7 @@ def transform_allah(name):
 
 
 nice_name_rules = [
-	#transform_small_letter,
 	transform_capital_letter,
+	transform_small_letter,
 	transform_allah,
 ]
