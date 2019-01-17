e4836c7192f03991b4f44a1e46c4bc75e4685ac2
Jens Kutilek <webmail@kutilek.de>
2019-01-17 11:49:01 +0100

    Add scripts data (not used yet)

--- lib/jkUnicode/data/updateUniData.sh
+++ lib/jkUnicode/data/updateUniData.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl -O http://www.unicode.org/Public/UNIDATA/UnicodeData.txt
 curl -O http://www.unicode.org/Public/UNIDATA/Blocks.txt
-#curl -O http://www.unicode.org/Public/UNIDATA/Scripts.txt
+curl -O http://www.unicode.org/Public/UNIDATA/Scripts.txt
 curl -O https://raw.githubusercontent.com/adobe-type-tools/agl-aglfn/master/aglfn.txt
