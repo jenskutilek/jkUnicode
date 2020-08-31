4cbf178ea943d283be3cbce3f16700447c1b73fd
Jens Kutilek <webmail@kutilek.de>
2020-08-31 17:28:55 +0200

    Version was already in use

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="jkUnicode",
-    version="1.5.0",
+    version="1.6.0",
     description="Unicode support libraries.",
     author="Jens Kutilek",
     url="http://www.kutilek.de/",
