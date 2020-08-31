3dad15403f523142b01b24f35b9ca599a0d6c790
Jens Kutilek <webmail@kutilek.de>
2020-08-31 17:23:08 +0200

    Use entry points instead of scripts

--- setup.py
+++ setup.py
@@ -3,33 +3,32 @@
 from setuptools import setup
 
 setup(
-	name = "jkUnicode",
-	version = "1.4.0",
-	description = "Unicode support libraries.",
-	author = "Jens Kutilek",
-	url = "http://www.kutilek.de/",
-	packages = [
-		"jkUnicode",
-		"jkUnicode.tools",
-	],
-	package_dir = {"": "lib"},
-	package_data = {"": [
-		"json/ignored.json",
-		"json/language_characters.json",
-		"json/languages.json",
-		"json/scripts.json",
-		#"json/tags.json",
-		"json/territories.json",
-	]},
-	install_requires = [
-		"fontTools",
-	],
-	scripts = [
-		"scripts/ortho",
-	],
-	#entry_points = {
-	#	"console_scripts": [
-	#		"ortho_scan="
-	#	],
-	#},
+    name="jkUnicode",
+    version="1.5.0",
+    description="Unicode support libraries.",
+    author="Jens Kutilek",
+    url="http://www.kutilek.de/",
+    packages=[
+        "jkUnicode",
+        "jkUnicode.tools",
+    ],
+    package_dir={"": "lib"},
+    package_data={
+        "": [
+            "json/ignored.json",
+            "json/language_characters.json",
+            "json/languages.json",
+            "json/scripts.json",
+            # "json/tags.json",
+            "json/territories.json",
+        ]
+    },
+    install_requires=[
+        "fontTools",
+    ],
+    entry_points={
+      "console_scripts": [
+          "ortho = jkUnicode.cmdline:generate_html"
+      ],
+    },
 )
