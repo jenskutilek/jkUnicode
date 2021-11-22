c6c674b7814f2f367b684474acc8d21699a200ef
Jens Kutilek <webmail@kutilek.de>
2021-11-22 12:29:12 +0100

    v1.7.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="jkUnicode",
-    version="1.7.0",
+    version="1.7.1",
     description="Unicode support libraries.",
     author="Jens Kutilek",
     url="http://www.kutilek.de/",
