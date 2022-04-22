cf0c523a780369ea1c2393283a0906523c1f2e40
Jens Kutilek <webmail@kutilek.de>
2022-04-22 13:06:35 +0200

    Update CLDR data to v41

--- lib/jkUnicode/data/updateLangData.sh
+++ lib/jkUnicode/data/updateLangData.sh
@@ -2,5 +2,5 @@
 # Check the URL if it is still current:
 # http://unicode.org/Public/cldr/
 # There seems to be no stable URL for the current version.
-curl -O http://unicode.org/Public/cldr/30.0.3/core.zip
+curl -O http://unicode.org/Public/cldr/41/core.zip
 #unzip core.zip -d core/
