519c4d173cf171075daa2e82bf251293664572a5
Jens Kutilek <webmail@kutilek.de>
2022-05-09 10:25:11 +0200

    Use setup.cfg and pyproject.toml

--- setup.py
+++ setup.py
@@ -1,35 +1,4 @@
-#! /usr/bin/env python
-
 from setuptools import setup
 
-setup(
-    name="jkUnicode",
-    version="1.8.0",
-    description="Unicode support libraries.",
-    author="Jens Kutilek",
-    url="http://www.kutilek.de/",
-    packages=[
-        "jkUnicode",
-        "jkUnicode.cmdline",
-        "jkUnicode.tools",
-    ],
-    package_dir={"": "lib"},
-    package_data={
-        "": [
-            "json/ignored.json",
-            "json/language_characters.json",
-            "json/languages.json",
-            "json/scripts.json",
-            # "json/tags.json",
-            "json/territories.json",
-        ]
-    },
-    install_requires=[
-        "fontTools",
-    ],
-    entry_points={
-      "console_scripts": [
-          "ortho = jkUnicode.cmdline.ortho:ortho"
-      ],
-    },
-)
+if __name__ == "__main__":
+    setup()
