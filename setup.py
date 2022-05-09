b17b2f8a68137ab201c9d89aaab0bbc8f24bc80a
Jens Kutilek <webmail@kutilek.de>
2022-05-09 15:09:41 +0200

    Build helpers.RangeDict with mypyc

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
+from mypyc.build import mypycify
+
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        ext_modules=mypycify([
+            "lib/jkUnicode/tools/helpers.py",
+        ]),
+    )
