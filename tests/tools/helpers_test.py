d675ce0186a94a2b67aa2d29ca63ea00c9a77576
Jens Kutilek <webmail@kutilek.de>
2023-03-07 18:27:16 +0100

    Add a test for RangeDict

---
+++ tests/tools/helpers_test.py
@@ -0,0 +1,19 @@
+import pytest
+import unittest
+from jkUnicode.tools.helpers import RangeDict
+
+
+class TestRangeDict(unittest.TestCase):
+    def test_existing_key(self):
+        rd = RangeDict({(1, 10): "Foo", (12, 13): "Bar"})
+        assert rd[1] == "Foo"
+
+    def test_unknown_key(self):
+        rd = RangeDict({(1, 10): "Foo", (12, 13): "Bar"})
+        with pytest.raises(KeyError):
+            _ = rd[0]
+
+    def test_none_key(self):
+        rd = RangeDict({(1, 10): "Foo", (12, 13): "Bar"})
+        with pytest.raises(KeyError):
+            _ = rd[None]
