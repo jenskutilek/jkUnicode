e41d24cfdfb3cbe8332a2304b6dd35fe7a9470a6
Jens Kutilek <webmail@kutilek.de>
2024-09-24 09:56:21 +0200

    Update CLDR URL to version 45

--- data/updateLangData.sh
+++ data/updateLangData.sh
@@ -3,7 +3,7 @@
 # Check the URL if it is still current:
 # http://unicode.org/Public/cldr/
 # There seems to be no stable URL for the current version.
-curl -O https://unicode.org/Public/cldr/42/core.zip
+curl -O https://unicode.org/Public/cldr/45/core.zip
 #unzip core.zip -d core/
 
 # BCP 47 language subtags
