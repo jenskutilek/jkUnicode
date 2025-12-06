ebc020466f90d65264bde3cf37b891a65082e77d
Jens Kutilek <webmail@kutilek.de>
2025-12-06 14:53:29 +0100

    Update fontTools stubs

--- stubs/fontTools/ttLib/__init__.pyi
+++ stubs/fontTools/ttLib/__init__.pyi
@@ -1,5 +1,12 @@
+from __future__ import annotations
+
 from io import BytesIO
-from typing import Optional, Union
+from typing import Any, List, Optional, TypedDict, Union
+
+from fontTools.ttLib.tables._g_l_y_f import table__g_l_y_f
+
+class KnownTable(TypedDict):
+    glyf: table__g_l_y_f
 
 class TTFont(object):
     def __init__(
@@ -19,4 +26,37 @@ class TTFont(object):
         quiet=None,
         _tableCache=None,
         cfg: dict = {},
+    ) -> None: ...
+    def __contains__(self, tag: str) -> bool: ...
+    def __delitem__(self, tag: str) -> None: ...
+    def __getitem__(self, tag: str) -> Any: ...
+    def __setitem__(self, tag: str, table: Any) -> None: ...
+    def close(self) -> None: ...
+    @property
+    def flavor(self) -> str: ...
+    @flavor.setter
+    def flavor(self, value: str) -> None: ...
+    def getBestCmap(
+        self,
+        cmapPreferences=(
+            (3, 10),
+            (0, 6),
+            (0, 4),
+            (3, 1),
+            (0, 3),
+            (0, 2),
+            (0, 1),
+            (0, 0),
+        ),
     ): ...
+    def getTableData(self, tag: str) -> bytes: ...
+    def importXML(
+        self, fileOrPath: Union[str, BytesIO], quiet: Optional[bool] = None
+    ): ...
+    def keys(self) -> List[str]: ...
+    def save(self, file: Union[str, BytesIO], reorderTables: bool = True) -> None: ...
+    def saveXML(
+        self, fileOrPath: Union[str, BytesIO], newlinestr: str = "\n", **kwargs
+    ): ...
+
+def newTable(tag): ...
