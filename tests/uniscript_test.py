8abfac9d7c518c77f9a4a2c4071fc2e2af350ce8
Jens Kutilek <webmail@kutilek.de>
2026-09-17 13:46:45 +0200

    Add more tests

--- tests/uniscript_test.py
+++ tests/uniscript_test.py
@@ -12,3 +12,9 @@ class TestUniScript(unittest.TestCase):
 
     def test_get_script_unknown(self) -> None:
         assert get_script(0xF000) == "Unknown"
+
+    def test_get_script_last(self) -> None:
+        assert get_script(0xE01EF) == "Inherited"
+
+    def test_get_script_past(self) -> None:
+        assert get_script(0xE01F0) == "Unknown"
